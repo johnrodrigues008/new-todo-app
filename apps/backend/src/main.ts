8532af019739d7a7a19429665aee4b376e810eef
John Rodrigues <john.rodrigues@gmail.com>
2025-07-21 02:14:17 -0300

    [Backend]- Alteração da função do cors corrigida.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,5 @@
+// src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,33 +7,46 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const whitelist = process.env.NODE_ENV === 'production'
+    ? [
+        'https://frontend-xi-two-20.vercel.app',
+        'https://new-todo-app-f1iq.onrender.com', 
+        'https://frontend-johnrodrigues008s-projects.vercel.app/'           
+      ]
+    : [
+        `http://localhost:${process.env.PORTORIGINCORS ?? 3000}`,
+        'https://frontend-xi-two-20.vercel.app',
+        'https://frontend-johnrodrigues008s-projects.vercel.app/'
+      ];
+
   app.enableCors({
-    origin: `http://localhost:${process.env.PORTORIGINCORS ?? 3000}`,
+    origin: (origin, callback) => {
+      if (!origin || whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS: Origem ${origin} não permitida`));
+      }
+    },
     credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    methods: ['GET','HEAD','POST','PUT','PATCH','DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
-  /* 
-    const allowedOrigins = process.env.NODE_ENV === 'production'
-    ? ['https://meusite.com']
-    : ['http://localhost:3000'];
-
-    app.enableCors({
-      origin: allowedOrigins,
-      credentials: true,
-    });
-
-  */
-
-  const config = new DocumentBuilder()
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Minha todo App API')
     .setDescription('API para o funcionamento do meu To do List.')
     .setVersion('1.0')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 4000);
+  // Inicia o servidor
+  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+  await app.listen(port);
+  console.log(`🚀 Servidor rodando em http://localhost:${port}`);
 }
+
 bootstrap();
